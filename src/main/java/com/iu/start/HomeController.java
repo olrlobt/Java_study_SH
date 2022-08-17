dd1abd52aae95ae4db69233b4917c0f86cfd6e7c
olrlobt <317tmdgjs@naver.com>
2022-08-17 15:09:20 +0900

    batis patch

--- src/main/java/com/iu/start/HomeController.java
+++ src/main/java/com/iu/start/HomeController.java
@@ -4,8 +4,10 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@Autowired
+	private SqlSession sqlSession;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
@@ -33,6 +38,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		System.out.println(sqlSession.getConnection());
 		return "home";
 	}
 	
